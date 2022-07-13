d76cc6d68b205be8faa3ba8b8626216def6e23c6
Paulo Serrano <pvsserrano@gmail.com>
2022-07-13 12:33:49 -0300

    Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,19 @@
 const Header = () => {
     return (
-        <div>
-            Header
+        <div className="header-wrapper">
+            <div class="logo-container">
+                <ion-icon name="logo-instagram"></ion-icon>
+                <img src="/images/logo.png" alt="instagram" />
+            </div>
+            <form action="/">
+                <input id="search" type="text" placeholder="Pesquisar" />
+            </form>
+            <nav class="nav-icons">
+                <ion-icon name="paper-plane-outline"></ion-icon>
+                <ion-icon name="compass-outline"></ion-icon>
+                <ion-icon name="heart-outline"></ion-icon>
+                <ion-icon name="person-outline"></ion-icon>
+            </nav>
         </div>
     )
 }
